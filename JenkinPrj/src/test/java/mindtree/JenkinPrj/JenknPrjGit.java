c887c2f931d80eb991f71d4023bb2950bb4ec0c8
PurushothSuresh <purushothaman.sureshkumar@mindtree.com>
2022-04-29 09:58:57 +0530

    First commit

---
+++ JenkinPrj/src/test/java/mindtree/JenkinPrj/JenknPrjGit.java
@@ -0,0 +1,12 @@
+package mindtree.JenkinPrj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JenknPrjGit {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","E:\\Work\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+}
